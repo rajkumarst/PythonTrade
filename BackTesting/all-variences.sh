c5e3af09656ba50edf21580c7e271bea4ad1b404
Somasundaram M <advaitham@gmail.com>
2018-04-06 02:14:04 +0530

    Use right python script

--- BackTesting/all-variences.sh
+++ BackTesting/all-variences.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-python2.7 Open-Equal-Low.today.py --varience Always-Buy --profit 1 --sl 0.5 --verbose > out.Always-buy.$$
-python2.7 Open-Equal-Low.today.py --varience Both-Buy-And-Sell --profit 1 --sl 0.5 --verbose > out.Both.$$
-python2.7 Open-Equal-Low.today.py --varience Go_with_Nifty --profit 1 --sl 0.5 --verbose > out.Nifty.$$
+python2.7 Open-Equal-Low.py --Always-Buy --profit 1 --sl 0.5 --verbose > out.Always-buy.$$
+python2.7 Open-Equal-Low.py --Both-Buy-And-Sell --profit 1 --sl 0.5 --verbose > out.Both.$$
+python2.7 Open-Equal-Low.py --Go_with_Nifty --profit 1 --sl 0.5 --verbose > out.Nifty.$$
--- BackTesting/today.sh
+++ BackTesting/today.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-python2.7 Open-Equal-Low.today.py --today --varience Always-Buy --profit 1 --sl 0.5 --verbose > out.today.always-buy.$$
-python2.7 Open-Equal-Low.today.py --today --varience Both-Buy-And-Sell --profit 1 --sl 0.5 --verbose > out.today.Both.$$
-python2.7 Open-Equal-Low.today.py --today --varience Go_with_Nifty --profit 1 --sl 0.5 --verbose > out.today.Nifty.$$
+python2.7 Open-Equal-Low.py --today --varience Always-Buy --profit 1 --sl 0.5 --verbose > out.today.always-buy.$$
+python2.7 Open-Equal-Low.py --today --varience Both-Buy-And-Sell --profit 1 --sl 0.5 --verbose > out.today.Both.$$
+python2.7 Open-Equal-Low.py --today --varience Go_with_Nifty --profit 1 --sl 0.5 --verbose > out.today.Nifty.$$
